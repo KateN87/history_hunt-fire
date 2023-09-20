6489c1d290033e8333f44add12e7f40816eabf64
Kate <kate.nordqvist@gmail.com>
2023-09-20 11:10:28 +0200

    test

--- firebase/config.js
+++ firebase/config.js
@@ -3,7 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+	apiKey: "AIzaSyDIAFqtpBRFuwVDz-IWKuW141zp5gUc7TI",
+	authDomain: "historyhunt-a8c4b.firebaseapp.com",
+	projectId: "historyhunt-a8c4b",
+	storageBucket: "historyhunt-a8c4b.appspot.com",
+	messagingSenderId: "287021838956",
+	appId: "1:287021838956:web:388a33c7899146fae19e49",
+};
 
 //initialize firebase
 initializeApp(firebaseConfig);
